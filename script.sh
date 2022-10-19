5668809fd6c1e895ff2610513cf00523f85e8fa8
mfwramdev <98235691+mfwramdev@users.noreply.github.com>
2022-10-19 23:09:32 +0530

    Fixed parsing space

--- script.sh
+++ script.sh
@@ -9,12 +9,12 @@ if [ -f "config" ]; then
 	fi
 	echo "Hello "$usrname
 	webhk=`sed '2q;d' config`
-	echo "Message:"; read mesg
+	echo "Message:"; read -r -p mesg
 else
 	echo "[TIP]You can create a file named config and put your name in the first line and webhook in the second line"
-	echo "Username:"; read usrname
-	echo "Message:"; read mesg
-	echo "Webhook:"; read webhk
+	echo "Username:"; read -r -p usrname
+	echo "Message:"; read -r -p mesg
+	echo "Webhook:"; read -r -p webhk
 fi
 
 if [[ $(curl $webhk) ]] 2>/dev/null;
